a28c9e7744bb4b33e2f06d93457525ffab41856b
stevemqeen <stevemqeen@gmail.com>
2014-07-28 04:38:57 +0300

    + GFXLoad filter added. + started search DDS/GFX loading functions.

--- src/zClient/zMain.cpp
+++ src/zClient/zMain.cpp
@@ -20,6 +20,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 			// ----
 			gGlow.isEnabled = GetPrivateProfileInt("GLOW", "Enabled", 0, "./Option.ini"); // I just put it here
 			// ----
+			//009C470A   E8 7B3FF6FF      CALL main.0092868A - DDS file info
 			MessageBox(0, "Loaded", "Caption", 0); //Test purposes, so I know if dll is loaded!
 		}
 		break;
